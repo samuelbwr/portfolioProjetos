3d193649d358f5ed19c45c00cb16fa8623da17dd
Samuel Berwanger <samuelbwr@gmail.com>
2017-07-22 10:12:14 -0300

    updating project names and dependencies

--- spring-boot-jsp-jpa/src/main/java/br/com/projetos/controller/IndexController.java
+++ spring-boot-jsp-jpa/src/main/java/br/com/projetos/controller/IndexController.java
@@ -1,4 +1,4 @@
-package br.com.biblioteca.controller;
+package br.com.projetos.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
